e25ca90cef494e0da5b321315b8cd2b08fae0293
Yauhen Kharuzhy <jekhor@gmail.com>
2013-04-28 20:58:24 +0300

    Initial commit
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HsInventory::Application.config.secret_token = 'a68d87ac820fd782d2fb4455a152c1242b26e0ef78422a24e924f1f75ca8d7a9bb12e01b39b18c5d62823bab23041552e1278ddd117b5d777a01b07efbbe6b0e'
